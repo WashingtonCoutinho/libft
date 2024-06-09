49e06cef5ba46be2f1833480be1d36abcd8e0cee
Washington Da Silva Duraes Coutinho <wada-sil@c1r14s13.42lisboa.com>
2024-06-09 18:18:43 +0100

    :bug:fixing bugs

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,15 +6,23 @@
 /*   By: wada-sil <wada-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 13:27:43 by wada-sil          #+#    #+#             */
-/*   Updated: 2024/06/08 20:01:36 by wada-sil         ###   ########.fr       */
+/*   Updated: 2024/06/09 18:17:20 by wada-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_isprint(int n)
 {
-	if (n >= 33 && n <= 127)
+	if (n >= 32 && n <= 127)
 	{
 		return (1);
 	}
 	return (0);
 }
+
+/* int main(void)
+{
+	char str = 123;
+	printf("%i\n", ft_isprint(str));
+} */
